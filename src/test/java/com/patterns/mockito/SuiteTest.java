8f4b502d6c8ae5bf78c9c984ecfe4ab8bd1f0591
Inma Jimenez <inmalmeda@hotmail.com>
2021-03-09 11:49:55 +0100

    ArregloSuiteTestMockito

--- src/test/java/com/patterns/mockito/SuiteTest.java
+++ src/test/java/com/patterns/mockito/SuiteTest.java
@@ -1,5 +1,13 @@
 package com.patterns.mockito;
 
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.SuiteDisplayName;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("com.patterns.mockito")
+@SuiteDisplayName("Suite patrones")
 public class SuiteTest {
 
 
